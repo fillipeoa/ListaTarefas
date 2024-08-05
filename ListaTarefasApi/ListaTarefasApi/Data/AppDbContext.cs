d51c8a7d5bc3d4dbf15c80f22134062193f53fe2
Fillipe Almeida <62079631+fillipeoa@users.noreply.github.com>
2024-08-05 10:41:08 -0300

    feat: substituido projeto .net por gerado no Visual Studio

---
+++ ListaTarefasApi/ListaTarefasApi/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿// Data/AppDbContext.cs
+
+using Microsoft.EntityFrameworkCore;
+using ListaTarefasApi.Models;
+
+namespace ListaTarefasApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ ListaTarefasApi/ListaTarefasApi/Program.cs
@@ -0,0 +1,33 @@
+using ListaTarefasApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
